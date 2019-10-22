6e64fde938a1937fc2c169e893200cb477169e27
tt0417 <rpreis@novobanco.pt>
2019-10-23 00:48:47 +0100

    Ajustes do documento

--- lr_adenda.py
+++ lr_adenda.py
@@ -6,8 +6,6 @@ Avaliação dos últimos 150 dias
 """
 
 import pickle
-import numpy as np
-import matplotlib.pyplot as plt
 from recveg import Composito, Utilidades
 
 def executa_calculo(nome_ficheiro):
@@ -19,13 +17,19 @@ def executa_calculo(nome_ficheiro):
     modelo = pickle.load(ficheiro, encoding='latin1')
     dados = modelo["tabela"]
 
-    tempo = dados[:-10, Composito.DATA_FIM]
-    alvo = dados[:-10, Composito.NDVI_MEDIA_VEGETACAO_ALVO]
-    referencia = dados[:-10, Composito.NDVI_MEDIA_VEGETACAO_ALVO]
+    maximo = dados[0, Composito.NDVI_MEDIA_VEGETACAO_ALVO]-\
+        dados[0, Composito.NDVI_MEDIA_VEGETACAO_REFERENCIA]
+    tempo = dados[-10:, Composito.DATA_FIM]
+    alvo = dados[-10:, Composito.NDVI_MEDIA_VEGETACAO_ALVO]
+    referencia = dados[-10:, Composito.NDVI_MEDIA_VEGETACAO_REFERENCIA]
     diferenca = alvo-referencia
     
-    for elemento in tempo:
-        print(Utilidades.formata_data(elemento))
+    for elemento in range(10):
+        print("%s & %3.1f\\%% & %5.4f & %5.4f & %5.4f\\\\" % (Utilidades.formata_data(tempo[elemento]),\
+            100 * diferenca[elemento] / maximo,\
+            alvo[elemento],\
+            referencia[elemento],\
+            diferenca[elemento]))
 
 # Atualizar com o nome do ficheiro que se pretende utilizar
-executa_calculo("dados/pedrogao-5.ndvi")
+executa_calculo("dados/pedrogao-atual.ndvi")
